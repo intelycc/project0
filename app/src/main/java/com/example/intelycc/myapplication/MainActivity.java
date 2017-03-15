a09d1a230336e07e801a513d734ade490ed2b0f8
ycc <intelycc@163.com>
2017-03-15 10:52:35 +0800

    添加功能

--- app/src/main/java/com/example/intelycc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intelycc/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.intelycc.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int toastDuration = Toast.LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButtonClick(View view){
+        Button button = (Button)view;
+        Toast toast = Toast.makeText(this, "启动应用:"+ button.getText(), toastDuration);
+        toast.show();
+    }
+
+
 }
